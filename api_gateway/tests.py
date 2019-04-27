8f9c52f41739e0b32eb7d013a4fca3ad0cff4d04
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-04-27 12:31:51 -0300

    [REFACTOR] Testado metodo que retorna todas as monitorias.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: MatheusEstanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: lukassxp <lpalucas.10@gmail.com>

--- api_gateway/tests.py
+++ api_gateway/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from api_gateway.views import all_tutoring
+import requests_mock
+from rest_framework.test import APITestCase
+import json
 
-# Create your tests here.
+from rest_framework.status import (
+    HTTP_403_FORBIDDEN,
+    HTTP_200_OK,
+    HTTP_404_NOT_FOUND,
+    HTTP_400_BAD_REQUEST,
+    HTTP_500_INTERNAL_SERVER_ERROR
+)
+
+class MonitoringRedirectTests(APITestCase):
+    # def setUp(self):
+    #     self.valid_payload = {
+    #         'name': 'teste'
+    #     }
+
+    #     self.invalid_payload = {
+    #         'name': ''
+    #     }
+    
+    @requests_mock.Mocker(kw='mock')
+    def test_all_tutoring(self, **kwargs):
+        api_url = '/all_tutoring/'
+        request_url = 'http://api-monitoria:8001/tutoring/'
+        status = HTTP_200_OK
+        data = {"Teste": "teste"}
+
+        kwargs['mock'].get(request_url, text=json.dumps(data))
+
+        response = self.client.get(api_url)
+
+        self.assertEqual(response.status_code, status)
+        self.assertEqual(response.data['Teste'], "teste")
+
+    def test_error_all_tutoring(self, **kwargs):
+        api_url = '/all_tutoring/'
+        status = HTTP_500_INTERNAL_SERVER_ERROR
+        data = {'error': 'Error no servidor'}
+
+        response = self.client.get(api_url)
+
+        self.assertEqual(response.status_code, status)
+        self.assertEqual(response.data, data)
