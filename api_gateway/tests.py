0a6db353622a742155664a6456d4e540ee470416
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-04-27 20:24:44 -0300

    [REFACTOR] Testado metodo que retorna uma monitoria caso falho.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: MatheusEstanislau <matheus.estanislau7@gmail.com>

--- api_gateway/tests.py
+++ api_gateway/tests.py
@@ -50,15 +50,25 @@ class MonitoringRedirectTests(APITestCase):
         id = self.valid_payload
         api_url = '/get_tutoring/'
         request_url = 'http://api-monitoria:8001/tutoring/1/'
+
         status = HTTP_200_OK
-        data = {'error': 'Error no servidor'}
+        data = {'Teste': 'teste'}
         kwargs['mock'].get(request_url,text = json.dumps(data))
 
         response = self.client.post(api_url,id)
         self.assertEqual(response.status_code, status)
-        self.assertEqual(response.data['error'] ,'Error no servidor'  )
+        self.assertEqual(response.data['Teste'] ,'teste'  )
 
+    def teste_error_get_tutoring(self, **kwargs):
+        id = self.valid_payload
+        api_url = '/get_tutoring/'
 
+        status = HTTP_500_INTERNAL_SERVER_ERROR
+        data = {'error': 'Error no servidor'} 
+        response = self.client.post(api_url,id)
+        self.assertEqual(response.status_code, status)
+        self.assertEqual(response.data ,data  )
+        
 
 
     # @requests_mock.Mocker(kw='mock')
