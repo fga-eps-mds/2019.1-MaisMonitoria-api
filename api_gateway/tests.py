5b42bc4371cd52978f902f71cc8e73b1933dbadd
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-04-28 19:50:20 -0300

    [REFACTOR] Testado função criar monitoria.
    Signed-off-by: Matheus Estanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: jpcirqueira <jpcirqueira81@gmail.com>
     Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>

--- api_gateway/tests.py
+++ api_gateway/tests.py
@@ -76,7 +76,7 @@ class MonitoringRedirectTests(APITestCase):
         request_url = 'http://api-monitoria:8001/tutoring/1/'
         data = {"Success":"Excluido com sucesso"}
         status = HTTP_200_OK
-
+        
         kwargs['mock'].delete(request_url, text= json.dumps(data))
         response = self.client.post(api_url, id)
         self.assertEqual(response.status_code, status)
@@ -90,4 +90,27 @@ class MonitoringRedirectTests(APITestCase):
         response = self.client.post(api_url, id)
         self.assertEqual(response.status_code, status)
         self.assertEqual(response.data, data)
-  
+    
+    
+    @requests_mock.Mocker(kw= 'mock')    
+    def teste_create_tutoring(self, **kwargs):
+        
+        id= self.valid_payload
+        api_url = '/create_tutoring/'
+        request_url = 'http://api-monitoria:8001/tutoring/'
+        data = {"Success":"Criado com sucesso"}
+        status = HTTP_200_OK
+
+        kwargs['mock'].post(request_url, text= json.dumps(data))
+        response = self.client.post(api_url, id)
+        self.assertEqual(response.status_code, status)
+        self.assertEqual(response.data, data)
+
+    def teste_error_create_tutoring(self, **kwargs):
+        id= self.valid_payload
+        api_url = '/create_tutoring/'
+        data = {'error': 'Error no servidor'}
+        status =HTTP_500_INTERNAL_SERVER_ERROR
+        response = self.client.post(api_url, id)
+        self.assertEqual(response.status_code, status)
+        self.assertEqual(response.data, data)
