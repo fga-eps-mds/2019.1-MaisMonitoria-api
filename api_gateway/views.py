cdfe4fba3d505090b749d8a88704334f564f17b3
lukassxp <lpalucas.10@gmail.com>
2019-04-29 02:38:45 -0300

    [REFACTOR] Criados metodos para criar, alterar e visualizar monitoria.

--- api_gateway/views.py
+++ api_gateway/views.py
@@ -10,6 +10,7 @@ import os
 
 URL = 'http://api-monitoria:8001/'
 ROUTE = 'tutoring/'
+MONITOR_ROUTE = 'user/'
 
 @api_view(["POST"])
 def all_tutoring(request):
@@ -27,50 +28,50 @@ def all_tutoring(request):
 
 @api_view(["POST"])
 def get_tutoring(request):
-    ## Verificação do token
-    #decoded_token = auth.verify_id_token(request.data['access_token'])
-    #uid_json = {"id": decoded_token['uid'], "name": decoded_token['name']}
-    
-    #id = json.dumps(uid_json)
-    try:
-        response = requests.get('http://api-monitoria:8001' + '/tutoring/'+ 
-                                    str(request.data.get("id_tutoring_session"))+'/')
-        try:
-            respose_json = response.json()
-            return Response(data=respose_json, status=HTTP_200_OK)
-        except:
-            Response(response)
-    except:
-        return Response({'error': 'Error no servidor'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+    token = request.data['access_token']
+    auth_response = verify_auth(token)
+
+    if auth_response['is_auth']:
+        param = str(request.data['id_tutoring_session'])
+        return get_request(URL, ROUTE, param)
+    else:
+        respose_json = {
+            'error': 'Falha de autenticação'
+        }
+        return Response(data=json.dumps(respose_json), 
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(["POST"])
 def create_tutoring(request):
-    ## Verificação do token
-    #decoded_token = auth.verify_id_token(request.data['access_token'])
-    #uid_json = {"id": decoded_token['uid'], "name": decoded_token['name']}
-    
-    #id = json.dumps(uid_json)
-    try:
-        response = requests.post('http://api-monitoria:8001' + '/tutoring/', data=request.data)
-        try:
-            respose_json = response.json()
-            return Response(data=respose_json, status=HTTP_200_OK)
-        except:
-            Response(response)
-    except:
-        return Response({'error': 'Error no servidor'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+    token = request.data['access_token']
+    auth_response = verify_auth(token)
+
+    if auth_response['is_auth']:
+        data = request.data
+        data['monitor'] = URL + MONITOR_ROUTE + auth_response['id'] + '/'
+        del data['access_token']
+        return post_request(URL, ROUTE, data)
+    else:
+        respose_json = {
+            'error': 'Falha de autenticação'
+        }
+        return Response(data=json.dumps(respose_json), 
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(["POST"])
 def update_tutoring(request):
-    ## Verificação do token
-    #decoded_token = auth.verify_id_token(request.data['access_token'])
-    #uid_json = {"id": decoded_token['uid'], "name": decoded_token['name']}
-    
-    #id = json.dumps(uid_json)
-    try:
-        response = requests.put('http://api-monitoria:8001' + '/tutoring/'+
-                            str(request.data.get("id_tutoring_session"))+'/', data=request.data)
+    token = request.data['access_token']
+    auth_response = verify_auth(token)
 
-        return Response({"Success":"Alterado com sucesso"}, status=HTTP_200_OK)
-    except:
-        return Response({'error': 'Error no servidor'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+    if auth_response["is_auth"]:
+        param = str(request.data['id_tutoring_session'])
+        data = request.data
+        data['monitor'] = URL + MONITOR_ROUTE + auth_response['id'] + '/'
+        del data['access_token']
+        return put_request(URL, ROUTE, param, data)
+    else:
+        respose_json = {
+            'error': 'Falha de autenticação'
+        }
+        return Response(data=json.dumps(respose_json), 
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
