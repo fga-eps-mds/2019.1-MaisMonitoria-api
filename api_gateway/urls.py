44ba350d6cb797850be93aec861e883c6ff3f96e
lukassxp <lpalucas.10@gmail.com>
2019-04-29 00:50:45 -0300

    [REFACTOR] Disponibilizado metodo que retorna todas as monitorias.

--- api_gateway/urls.py
+++ api_gateway/urls.py
@@ -1,14 +1,9 @@
+from .views import all_tutoring, get_tutoring, create_tutoring, update_tutoring
 from django.urls import path
-from .views import all_tutoring
-from .views import get_tutoring
-from .views import delete_tutoring
-from .views import create_tutoring
-from .views import update_tutoring
 
 urlpatterns = [
     path('all_tutoring/', all_tutoring),
     path('get_tutoring/', get_tutoring),
-    path('delete_tutoring/', delete_tutoring),
     path('create_tutoring/', create_tutoring),
     path('update_tutoring/', update_tutoring)
 ]
--- api_gateway/views.py
+++ api_gateway/views.py
@@ -1,44 +1,29 @@
-from django.shortcuts import render
-from rest_framework import generics
+from api.utils import verify_auth, get_request, put_request, post_request
 from rest_framework.decorators import api_view
-from rest_framework.status import (
-    HTTP_403_FORBIDDEN,
-    HTTP_200_OK,
-    HTTP_404_NOT_FOUND,
-    HTTP_400_BAD_REQUEST,
-    HTTP_500_INTERNAL_SERVER_ERROR
-)
 from rest_framework.response import Response
+from django.shortcuts import render
+from rest_framework import status
+
 import requests
 import json
 import os
 
+URL = 'http://api-monitoria:8001/'
+ROUTE = 'tutoring/'
 
-import firebase_admin
-from firebase_admin import auth
-from firebase_admin import credentials
-
-# Initialize the default app
-#cred = credentials.Certificate('serviceAccountKey.json')
-#default_app = firebase_admin.initialize_app(cred)
-
-
-@api_view(["GET"])
+@api_view(["POST"])
 def all_tutoring(request):
-    ## Verificação do token
-    #decoded_token = auth.verify_id_token(request.data['access_token'])
-    #uid_json = {"id": decoded_token['uid'], "name": decoded_token['name']}
-    
-    #id = json.dumps(uid_json)
-    try:
-        response = requests.get('http://api-monitoria:8001' + '/tutoring/')
-        try:
-            respose_json = response.json()
-            return Response(data=respose_json, status=HTTP_200_OK)
-        except:
-            Response(response)
-    except:
-        return Response({'error': 'Error no servidor'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+    token = request.data['access_token']
+    auth_response = verify_auth(token)
+
+    if auth_response['is_auth']:
+        return get_request(URL, ROUTE)
+    else:
+        respose_json = {
+            'error': 'Falha de autenticação'
+        }
+        return Response(data=json.dumps(respose_json), 
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(["POST"])
 def get_tutoring(request):
@@ -55,20 +40,6 @@ def get_tutoring(request):
             return Response(data=respose_json, status=HTTP_200_OK)
         except:
             Response(response)
-    except:
-        return Response({'error': 'Error no servidor'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
-
-@api_view(["POST"])
-def delete_tutoring(request):
-    ## Verificação do token
-    #decoded_token = auth.verify_id_token(request.data['access_token'])
-    #uid_json = {"id": decoded_token['uid'], "name": decoded_token['name']}
-    
-    #id = json.dumps(uid_json)
-    try:
-        response = requests.delete('http://api-monitoria:8001' + '/tutoring/'+ 
-                                    str(request.data.get("id_tutoring_session"))+'/')
-        return Response({"Success":"Excluido com sucesso"}, status=HTTP_200_OK)
     except:
         return Response({'error': 'Error no servidor'}, status=HTTP_500_INTERNAL_SERVER_ERROR)
 
