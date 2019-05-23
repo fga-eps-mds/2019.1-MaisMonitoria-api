d693ddbde9a4fac605c4ea5a95ba0b3133223020
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-05-22 23:47:00 -0300

    [REFACTOR] Atualizado o teste test_get_user.

--- api_user/tests.py
+++ api_user/tests.py
@@ -1,6 +1,10 @@
-# import requests_mock
-# from rest_framework.test import APITestCase
-# import json
+from rest_framework.test import APITestCase
+from api_gateway.views import all_tutoring
+from rest_framework import status 
+import firebase_admin
+import requests_mock
+import json
+import mock
 
 # from rest_framework.status import (
 #     HTTP_403_FORBIDDEN,
@@ -10,30 +14,35 @@
 #     HTTP_500_INTERNAL_SERVER_ERROR
 # )
 
-# class ApiUserRedirectTests(APITestCase):
-#     def setUp(self):
-#         self.valid_payload = {
-#             'user_account_id':'1'
-#         }
+class ApiUserRedirectTests(APITestCase):
+    def setUp(self):
+        self.id={
+            'id': '1'
+        }
+        self.valid_payload = {
+             'access_token': '123'
 
-#         self.invalid_payload = {
-#             'user_account_id':''
-#         }
-    
-#     @requests_mock.Mocker(kw='mock')
-#     def test_get_user(self, **kwargs):
-#         id = self.valid_payload
-#         api_url = '/get_user/'
-#         request_url = 'http://api-monitoria:8001/user/1/'
-#         status = HTTP_200_OK
-#         data = {"Teste": "teste"}
+        }
 
-#         kwargs['mock'].get(request_url, text=json.dumps(data))
+        self.invalid_payload = {
+            'user_account_id':''
+        }
+   
+    @mock.patch('firebase_admin.auth.verify_id_token', mock.Mock(return_value={ 'uid': '1','id':'1'}))
+    @mock.patch('api.utils.verify_auth', mock.Mock(return_value={ 'id':'1' }))
+    @requests_mock.Mocker(kw='mock')
+    def test_get_user(self, **kwargs):
+        request_id = self.valid_payload
+        api_url = '/get_user/'
+        request_url = 'http://api-monitoria:8001/user/1'
+        request_status = status.HTTP_200_OK
+        data = {"Teste": "teste"}
 
-#         response = self.client.post(api_url,id)
+        kwargs['mock'].get(request_url, text=json.dumps(data))
 
-#         self.assertEqual(response.status_code, status)
-#         self.assertEqual(response.data['Teste'], "teste")
+        response = self.client.post(api_url,request_id)
+        self.assertEqual(response.status_code, request_status)
+        self.assertEqual(response.data['Teste'], "teste")
 
 #     def test_error_get_user(self, **kwargs):
 #         id = self.invalid_payload
